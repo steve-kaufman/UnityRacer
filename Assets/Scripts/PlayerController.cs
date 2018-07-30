a3a8440d0b9312cac4ebba8d09d117d30b946019
Steven <steveplaysguiola@gmail.com>
2018-07-30 17:55:20 -0500

    initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerSub))]
+
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField]
+	private float sensitivity;
+	[SerializeField]
+	private float acceleration;
+	[SerializeField]
+	private float maxSpeed;
+	[SerializeField]
+	private float brakeSpeed;
+
+	
+	private PlayerSub motor;
+
+
+	// Use this for initialization
+	void Start () {
+		motor = GetComponent<PlayerSub>();
+		motor.SetMaxSpeed(maxSpeed);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(Input.GetAxisRaw("Vertical") == 1) motor.Accel(acceleration);
+		if(Input.GetAxisRaw("Vertical") == -1) motor.Brake(brakeSpeed);
+		motor.Turn(Input.GetAxis("Horizontal") * sensitivity);
+	}
+	
+}
---
+++ Assets/Scripts/PlayerSub.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class PlayerSub : MonoBehaviour {
+
+	private float maxSpeed = 0f;
+
+
+	private Rigidbody rb;
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	public void Accel(float acceleration){
+		Vector3 localVelocity = transform.rotation * rb.velocity;
+		if(localVelocity.z >= maxSpeed) return;
+		print(localVelocity.z);
+		Vector3 force = rb.rotation * new Vector3(0f, 0f, -acceleration / 10);
+		//force = force.normalized * Time.deltaTime;
+		rb.velocity += force;
+	}
+
+	public void Brake(float brakeSpeed){
+		Vector3 localVelocity = transform.rotation * rb.velocity;
+		if(localVelocity.z <= 0) return;
+		Vector3 brakeVector = rb.rotation * new Vector3(0f, 0f, -brakeSpeed / 10);
+		rb.velocity -= brakeVector;
+	}
+
+	public void SetMaxSpeed(float _maxSpeed){
+		maxSpeed = _maxSpeed;
+	}
+
+	public void Turn(float turnFactor){
+		Vector3 _turn = new Vector3(0f, turnFactor / 10, 0f);
+		rb.MoveRotation(rb.rotation * Quaternion.Euler(_turn));
+	}
+
+}
